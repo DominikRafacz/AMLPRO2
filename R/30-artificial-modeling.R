d50e27e417e20b891e2af94429365e86a03eff16
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-05-30 14:04:32 +0200

    artificial plotting

--- R/30-artificial-modeling.R
+++ R/30-artificial-modeling.R
@@ -5,6 +5,7 @@ library(e1071)
 library(ranger)
 library(MASS)
 library(progressr)
+library(forcats)
 
 task_artificial <- TaskClassif$new(
   id = "artificial",
@@ -24,28 +25,39 @@ learners <- lrns(
   predict_type = "prob"
 )
 
-selected_features_sum_wo_mRMR <- c(49, 106, 129, 242, 339, 425, 476)
-selected_features_mRMR <- c(91, 229, 277, 405, 424)
+selected_features_sum_wo_mRMR <- paste0("X", c(49, 106, 129, 242, 339, 425, 476))
+selected_features_mRMR <- paste0("X",c(91, 229, 277, 405, 424))
+selected_features_reduced <- setdiff(selected_features_sum_wo_mRMR, paste0("X", c(129, 476)))
 
-task_artificial_sum_wo_mRMR <- task_artificial$clone()$select(cols = paste0("X", selected_features_sum_wo_mRMR))
+task_artificial_sum_wo_mRMR <- task_artificial$clone()$select(cols = selected_features_sum_wo_mRMR)
 task_artificial_sum_wo_mRMR$id <- "artificial_sum_wo_mRMR"
-task_artificial_sum_w_mRMR <- task_artificial$clone()$select(cols = paste0("X", c(selected_features_sum_wo_mRMR, selected_features_mRMR)))
+task_artificial_sum_w_mRMR <- task_artificial$clone()$select(cols = c(selected_features_sum_wo_mRMR, selected_features_mRMR))
 task_artificial_sum_w_mRMR$id <- "artificial_sum_w_mRMR"
-task_artificial_only_mRMR <- task_artificial$clone()$select(cols = paste0("X", selected_features_mRMR))
+task_artificial_only_mRMR <- task_artificial$clone()$select(cols = selected_features_mRMR)
 task_artificial_only_mRMR$id <- "artificial_only_mRMR"
+task_artificial_reduced <- task_artificial$clone()$select(cols = selected_features_reduced)
+task_artificial_reduced$id <- "artificial_reduced"
 
 resampling <- rsmp("cv", folds = 5)
 design <- benchmark_grid(list(task_artificial_sum_wo_mRMR,
                               task_artificial_sum_w_mRMR,
-                              task_artificial_only_mRMR),
+                              task_artificial_only_mRMR,
+                              task_artificial_reduced),
                          learners, resampling)
 
 set.seed(420)
 results <- benchmark(design)
-results$aggregate(msrs(c("classif.acc", "classif.auc")))
+results$aggregate(msrs(c("classif.acc", "classif.auc", "classif.bacc")))
 
-results$score(msrs(c("classif.acc", "classif.auc"))) %>%
-  ggplot(aes(x = task_id, y = classif.acc)) +
+results$score(msrs(c("classif.acc", "classif.auc", "classif.bacc"))) %>%
+  ggplot(aes(x = fct_relevel(factor(task_id), c("artificial_sum_w_mRMR", "artificial_only_mRMR", "artificial_sum_wo_mRMR", "artificial_reduced")), y = classif.bacc)) +
   geom_boxplot() +
   facet_wrap(~learner_id) +
-  theme_bw()
+  theme_bw() +
+  scale_x_discrete(
+    name = "selected features",
+    labels = c("sum of all", "only mRMR", "without mRMR, without correlated", "without mRMR")) +
+  scale_y_continuous("balanced accuracy") +
+  ggtitle("Comparison of BA for different models for different selection of features", subtitle = "results for 5 iterations of crossvalidation")
+
+ggsave("plots/BA-artificial.png")
